57f05f2be8780e24702cef9fae85e92d8eabbf0a
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-27 13:59:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
   end
   def get_page
     doc=Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    bindign.pry
+    binding.pry
   end 
   def get_courses
     self.get_page.css(".post")
